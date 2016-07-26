71fc3184a14e63e60ad54367816b698f4f831b80
Nazim Bellahsene <nazim.bellahsene@gmail.com>
2016-07-26 16:07:36 -0700

    xx

--- server/models/story.js
+++ server/models/story.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
+
 var StorySchema = new mongoose.Schema({
     title: {type: String},
     genre: {type: String},
-    _snippet: [{type:Schema.Types.ObjectID, ref:'Snippetdb'}]
+    _snippet: [{type: mongoose.Types.ObjectId, ref:'Snippetdb'}]
 }, {timestamps: true});
 
 mongoose.model('Storydb', StorySchema);
