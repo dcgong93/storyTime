a9cf8c66c5d38f06ca4f0be0804e422bb450ed31
Nazim Bellahsene <nazim.bellahsene@gmail.com>
2016-07-26 12:54:50 -0700

    working on logreg

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('Myapp', ['ngRoute']);
+var myApp = angular.module('Myapp', ['ngRoute', 'ngCookies']);
 // We instantiate our application and we inject ngrouter so that it's available
 // and so that we can use it to set up our routes below. 
 
@@ -13,6 +13,11 @@ var myApp = angular.module('Myapp', ['ngRoute']);
 			{
 				controller: 'indexController',
 				templateUrl: "partials/index.html"
+			})
+			.when('/user/new',
+			{
+				controller: 'reglogController',
+				templateUrl: 'partials/userNew.html'
 			})
 	})
 }());
---
+++ server/models/user.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+var userSchema = new mongoose.Schema({
+    fname: {
+    	type: String,
+    	required: [true, "Frist Name can't be empty"]
+    },
+    lname: {
+    	type: String,
+    	required: [true, "Last Name can't be empty"]
+    },
+    email: {
+    	type: String,
+    	required: [true, "Email can't be empty"]
+    },
+    phone:{
+    	type: String,
+    	required: [true, "Phone can't be empty"]
+    },
+    location:{
+    	type: String,
+    	required: [true, "Location can't be empty"]
+    },
+    password:{
+    	type:String,
+    	required: [true, "Password can't be empty"],
+    	minlength: 8,
+    	maxlength: 32,
+    	// validate: {
+    	// 	validator: function(value){
+    	// 		return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,32}/.test( value );
+    	// 	},
+    	// 	message: "Password failed Validation, You must have at least 1 number, uppercase and special character"
+    	// }
+    }
+
+},{timestamps: true});
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.pre('save', function(done) {
+    this.password = this.generateHash(this.password);
+    done();
+});
+
+mongoose.model('User', userSchema);
