c7bceb5de094d097efb1991ae88f15ad4f3897bd
Danny Nguyen <danhn142@gmail.com>
2016-07-26 18:14:55 -0700

    Still working on the nav bar and image upload

---
+++ client/controllers/userController.js
@@ -0,0 +1,9 @@
+myApp.controller('userController', function($scope, dummyFactory){
+
+	console.log('I am able to load my userController along with my index partial');
+
+	// dummyFactory.addDummy({name: 'req.body.test', status: 'working'}, function(data){
+	// 	console.log(data);
+	// })
+	
+})
