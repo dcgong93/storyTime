a1999235ad1eef95680344f77ffae18fac52cdb8
Nazim Bellahsene <nazim.bellahsene@gmail.com>
2016-07-28 23:57:09 -0700

    update

--- client/controllers/new_storyController.js
+++ client/controllers/new_storyController.js
@@ -11,8 +11,9 @@ myApp.controller('new_storyController', ['$scope','$cookies','$location', 'Story
     console.log('client side new story controller');
     $scope.story._user = $cookies.getObject('auth')._id;
     StoryFactory.addStory($scope.story, function(data){
+    	console.log('this is the fucking story', data)
   		$scope.story = data;
-			console.log($scope.story[0]._id);
+			console.log('dont know',$scope.story[0]._id);
 			$location.path("/story/"+$scope.story[0]._id);
   	})
   }
